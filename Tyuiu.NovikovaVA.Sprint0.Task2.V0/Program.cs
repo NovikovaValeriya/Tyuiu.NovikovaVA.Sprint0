591abbe6a6cc35830117913c11e5d0591572609b
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-09-14 00:50:45 +0500

    Task 2

---
+++ Tyuiu.NovikovaVA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using Tyuiu.NovikovaVA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.NovikovaVA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Игорь"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+
+}
+

9cb162ab9d9c7d6c088af0838e7ddd32f866f5f8
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-09-15 21:38:01 +0500

    Task3

---
+++ Tyuiu.NovikovaVA.Sprint0.Tas3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.NovikovaVA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.NovikovaVA.Sprint0.Tas3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}

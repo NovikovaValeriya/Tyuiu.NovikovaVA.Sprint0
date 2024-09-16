bb1f0e959ee80df9cfd2d1ea061148fd81155218
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-09-16 21:09:06 +0500

    Task5

---
+++ Tyuiu.NovikovaVA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+using Tyuiu.NovikovaVA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.NovikovaVA.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
